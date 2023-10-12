2a42d28a46a636ee3cb119f9f529199393ce9eef
turall <tural_m@hotmail.com>
2023-10-12 17:51:35 +0400

    improve code base

--- index_update.py
+++ index_update.py
@@ -16,51 +16,94 @@ es = AsyncElasticsearch(**config)
 
 HOT_TIER = []
 COLD_TIER = []
+INDICES = dict()
 
 
 class LIMITS(float, Enum):
     HOT_TIER_SIZE_LIMIT = 600.0
     COLD_TIER_SIZE_LIMIT = 1200.0
-    HOT_INDEX_SIZE_LIMIT = 4
+    HOT_INDEX_SIZE_LIMIT = 100
 
 
-async def get_indexes_info() -> list[tuple]:
+def convert_size_to_bytes(size):
+    multipliers = {
+        "kb": 1024,
+        "mb": 1024 * 1024,
+        "gb": 1024 * 1024 * 1024,
+        "tb": 1024 * 1024 * 1024 * 1024,
+    }
+
+    for suffix in multipliers:
+        if size.lower().endswith(suffix):
+            return int(float(size[0 : -len(suffix)])) * multipliers[suffix]
+    else:
+        if size.lower().endswith("b"):
+            return int(size[0:-1])
+
+    try:
+        return int(size)
+    except ValueError:
+        print("Malformed input!")
+
+
+async def get_index_details():
+    indices = await es.indices.get(index="logstash-*")
+    index_info = indices.body
+    index_names = index_info.keys()
+    for i in index_names:
+        INDICES[i] = {"settings": index_info[i]["settings"]}
+
+
+async def get_indexes_sizes() -> list[tuple]:
     cluster_info = await es.cat.indices(index="logstash-*", s="index")
     index_info = cluster_info.body
     all_indexes = index_info.split("\n")
     all_indexes.pop()
     all_indexes = list(map(lambda index: index.split(), all_indexes))
-    return list(map(lambda x: (x[2], x[-1]), all_indexes))
+    for i in all_indexes:
+        INDICES[i[2]]["size"] = int(convert_size_to_bytes(i[-1]))
+
+
+def build_index_data():
+    indexes = []
+    for k, v in INDICES.items():
+        indexes.append({"size": v["size"], "settings": v["settings"], "index_name": k})
+    return indexes
 
 
 def parse_index_sizes(indexes_info: list[tuple]):
-    return list(map(lambda x: float(x[-1].replace("gb", "")), indexes_info))
+    val = 0
+    for v in indexes_info:
+        val += v.get("size")
+    return val
 
 
-async def get_index_settings(index_name: str):
-    settings = await es.indices.get_settings(index=index_name)
-    settings = settings.body
-    index_settings = settings[index_name]["settings"]["index"].pop("routing")
+def convert_byte_to_gb(size):
+    return size / (1 << 30)
+
+
+async def get_index_settings(index: dict):
+    index_settings = index["settings"]["index"].pop("routing")
     return index_settings["allocation"]["include"]["_tier_preference"]
 
 
-async def split_hot_cold_indexes(all_indexes):
-    for i in all_indexes:
-        tier = await get_index_settings(i[0])
+async def split_hot_cold_indexes(indexes):
+    for i in indexes:
+        tier = await get_index_settings(i)
         if tier == "data_content":
             HOT_TIER.append(i)
         else:
             COLD_TIER.append(i)
 
 
-def calculate_hot_tier_size():
-    sizes = parse_index_sizes(HOT_TIER)
-    return sum(sizes)
+def calculate_tier_size(indexes: list[dict]):
+    sizes = parse_index_sizes(indexes)
+    return convert_byte_to_gb(sizes)
 
 
-def calculate_cold_tier_size():
-    sizes = parse_index_sizes(COLD_TIER)
-    return sum(sizes)
+# def calculate_cold_tier_size():
+#     sizes = parse_index_sizes(COLD_TIER)
+#     return convert_byte_to_gb(sizes)
 
 
 def get_free_size_of_cold_tier(cold_instance_size: float, limit: float):
@@ -79,16 +122,26 @@ async def update_index_settings(index_name: str):
 
 
 def get_transferable_hot_indexes():
-    if len(HOT_TIER) > LIMITS.HOT_INDEX_SIZE_LIMIT:
-        diff = int(len(HOT_TIER) - LIMITS.HOT_INDEX_SIZE_LIMIT)
-        indexes = HOT_TIER[:diff]
-        return indexes
+    size = 0
+    index_count = 0
+    for i in HOT_TIER:
+        size += i["size"]
+        index_count += 1
+        size = convert_byte_to_gb(size)
+        if (
+            size > LIMITS.HOT_INDEX_SIZE_LIMIT
+            and (size - LIMITS.HOT_INDEX_SIZE_LIMIT) > 50
+        ):
+            break
+
+    indexes = HOT_TIER[:index_count]
+    return indexes
 
 
-async def transfer_index_from_hot_to_cold(hot_indexes_for_transfer: list[tuple]):
+async def transfer_index_from_hot_to_cold(hot_indexes_for_transfer: list[dict]):
     print("START transfer_index_from_hot_to_cold")
     for i in hot_indexes_for_transfer:
-        await update_index_settings(i[0])
+        await update_index_settings(i["index_name"])
 
 
 async def check_and_remove_cold_indexes(index_size: float):
@@ -111,7 +164,7 @@ async def check_and_remove_cold_indexes(index_size: float):
 
 async def check_cold_tier_limits(hot_index_sizes: float):
     print("START check_cold_tier_limits")
-    cold_index_size = sum(parse_index_sizes(COLD_TIER))
+    cold_index_size = calculate_tier_size(COLD_TIER)
     sum_of_with_hot_tier = cold_index_size + hot_index_sizes
     if sum_of_with_hot_tier <= LIMITS.COLD_TIER_SIZE_LIMIT:
         return True
@@ -120,12 +173,18 @@ async def check_cold_tier_limits(hot_index_sizes: float):
 
 
 async def main():
-    indexes = await get_indexes_info()
+    await get_index_details()
+    await get_indexes_sizes()
+    indexes = build_index_data()
     await split_hot_cold_indexes(indexes)
+    print(calculate_tier_size(HOT_TIER))
+    print(calculate_tier_size(COLD_TIER))
     transfer_hot_indexes = get_transferable_hot_indexes()
     if transfer_hot_indexes:
-        hot_index_size = sum(parse_index_sizes(transfer_hot_indexes))
+        hot_index_size = calculate_tier_size(transfer_hot_indexes)
         if transfer_hot_indexes and await check_cold_tier_limits(hot_index_size):
+            print(transfer_hot_indexes)
+            print(await check_cold_tier_limits(hot_index_size))
             await transfer_index_from_hot_to_cold(transfer_hot_indexes)
             print("Success!")
             return
